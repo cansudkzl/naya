2e04969fc57b78e6f0fd2bd1a4eccb297e2a0fc3
cansudkzl <156574302+cansudkzl@users.noreply.github.com>
2024-01-14 11:08:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById("btn")
+function changeTxt (){
+    btn.innerHTML = "Oooops";
+    btn.style.color = "red";
+}
+
+document.getElementById("btn"). addEventListener("click", changeTxt)
